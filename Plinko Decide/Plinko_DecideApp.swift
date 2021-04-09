8027393d34b1fd8f15f2338b8e772361bc46272b
Ilyas Munzir <ilyazmunzir@gmail.com>
2021-04-09 10:56:48 -0500

    Initial Commit

---
+++ Plinko Decide/Plinko_DecideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plinko_DecideApp.swift
+//  Plinko Decide
+//
+//  Created by Student on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Plinko_DecideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

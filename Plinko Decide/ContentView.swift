eeaafcec51d2c01dbea43175eeafa06c5688ad72
Ilyas Munzir <ilyazmunzir@gmail.com>
2021-04-12 10:51:30 -0500

    added options onto starting view and OptionsList class

--- Plinko Decide/ContentView.swift
+++ Plinko Decide/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var optionsList = OptionsList()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(optionsList.options) { option in
+                    Text(option.name)
+                }
+                .onMove(perform: { indices, newOffset in
+                    optionsList.options.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    optionsList.options.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("Plinko Decide")
+            .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
@@ -18,4 +33,9 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct OptionItem : Identifiable{
+    var id = UUID()
+    var name = ""
 }
---
+++ Plinko Decide/OptionsList.swift
@@ -0,0 +1,11 @@
+//
+//  OptionsList.swift
+//  Plinko Decide
+//
+//  Created by Student on 4/12/21.
+//
+
+import Foundation
+class OptionsList : ObservableObject {
+    @Published var options = [OptionItem(name: "Korean"), OptionItem(name: "American"), OptionItem(name: "Chinese")]
+}
